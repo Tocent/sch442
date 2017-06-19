227de88367e610e07e23d6cd1a522f1ebfd6d9ad
Tocent <dj@b-cph.com>
2017-06-19 21:54:06 +0200

    Add files via upload

---
+++ session_01.php
@@ -0,0 +1,33 @@
+
+<!doctype html>
+<html lang='en'>
+<head>
+	<meta charset ='utf-8'>
+	<title>Session test</title>
+</head>
+<body>
+	
+<form method='post' action='session_02.php'>
+
+
+<p>
+ <label for='first_name'> enter your firstname: </label>
+ <input type='text' name='first_name' id='first_name'>
+ </p>
+  <p>
+   <label for='drop_down> Vælg</label>
+   <option type= '' name='art_director' id='art_director>
+   <option type=''  name='tech' id='tech>
+   <option type=''  name='design' id='tech'>
+   <option type=''  name='communication' id='communcation'>
+ </p>
+ <br>
+ <p>
+  <input type='submit' name='submit' value='Submit'>
+  </p>
+</form>
+
+</body>
+</html>
+
+
